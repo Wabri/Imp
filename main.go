71b54e71e3d2e3477bbe984f20936bcfd5debbf5
Wabri <12409541+Wabri@users.noreply.github.com>
2023-02-16 23:01:11 +0100

    feat(cli): init cli with cobra
    I've run the `go mod imp` then `cobra-cli init`

--- main.go
+++ main.go
@@ -1,27 +1,13 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"fmt"
-
-	"imp/addons/gitlab"
-)
+import {
+	"imp/cmd"
+}
 
 func main() {
-
-}
-
-func testGitlabGetProject() {
-    fmt.Println(gitlab.GetProjectById(14355))
-}
-
-func testGitlabGetProjectHooks() {
-    fmt.Println(gitlab.GetProjectHooksById(14355))
-}
-
-func testGitlabPutProjectHooks() {
-    hook := gitlab.GetProjectHooksById(14355)[0]
-    hook.Url = "https://ciao.test"
-    hook.SslEnable = false
-    gitlab.PutProjectHooksById(14355, hook)
-    fmt.Println(gitlab.GetProjectHooksById(14355)[0])
+	cmd.Execute()
 }
