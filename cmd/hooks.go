7faafb5bf81f1f9307e39c88530bf4af31b5c071
Wabri <12409541+Wabri@users.noreply.github.com>
2023-02-17 15:22:58 +0100

    feat(cmd): update with more infos help page

--- cmd/hooks.go
+++ cmd/hooks.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2023 Gabriele Puliti <gabriele.puliti+github@proton.me>
 
 */
 package cmd
@@ -13,13 +13,8 @@ import (
 // hooksCmd represents the hook command
 var hooksCmd = &cobra.Command{
 	Use:   "hooks",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Get list of gitlab project hooks",
+	Long: `Get list of gitlab project hooks`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("hooks called")
 	},
@@ -27,14 +22,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	projectCmd.AddCommand(hooksCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// hookCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// hookCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
