21f574d2ea7024bbf843122810b8730527fa822b
Wabri <12409541+Wabri@users.noreply.github.com>
2023-04-03 18:24:07 +0200

    feat(artifactory): get list with infos
    Closes #38

--- cmd/artifactory.go
+++ cmd/artifactory.go
@@ -15,45 +15,57 @@ var artifactoryAction string
 var artifact string
 var repository string
 
-// gitlabCmd represents the gitlab command
 var artifactoryCmd = &cobra.Command{
 	Use:   "artifactory",
 	Short: "Artifactory service management",
-	Long: `Reach your artifactory service.`,
+	Aliases: []string{"arti", "a"},
+	Example: `
+	- Default: Get the list of all artifacts on specified repository
+	    imp artifactory --repository test-local
+	    imp artifactory --repository test-local --artifact test/local/1.2.3/
+	- action: repository-list -> Get the list of all repositories
+	    imp artifactory --action repository-list
+	- action: artifact-list -> Get the list of all artifacts on specified repository
+	    imp artifactory --action artifact-list test-local/test/local/1.2.3
+	    imp artifactory --repository test-local --action artifact-list 
+	- action: not-used-since -> List all the artifact of a repository not used by a specified date
+	    imp artifactory --repository test-local --action not-used-since 2023-03-14
+	- action: delete-artifact -> Remove the artifact on a specified repository
+	    imp artifactory --repository test-local --artifact test/local/1.2.3/artifact.json --action delete-artifact
+	- action: get-info -> Get all the infos about specific repository or artifact
+	    imp artifactory --repository test-local --action get-info
+	    imp artifactory --repository test-local --action get-info test/local/1.2.3
+	`,
 	Run: artifactoryRun,
 }
 
 func init() {
 	rootCmd.AddCommand(artifactoryCmd)
 	artifactoryCmd.Flags().StringVar(&artifact, "artifact", "", `Set artifact`)
-	artifactoryCmd.PersistentFlags().StringVar(&repository, "repository", "", `Set repository`)
-	artifactoryCmd.Flags().StringVar(&artifactoryAction, "action", "", `Action to do with hook: 
-	- artifact-list -> Get the list on a specified repository
-	    e.g.: imp artifactory --repository test-local --action artifact-list test/local/1.2.3/
-	- not-used-since -> List all the artifact of a repository not used by a specified date
-	    e.g.: imp artifactory --repository test-local --action not-used-since 2023-03-14
-	- delete-artifact -> Remove the artifact on a specified repository
-	    e.g.: imp artifactory --repository test-local --artifact test/local/1.2.3/artifact.json --action delete-artifact
-	`)
+	artifactoryCmd.Flags().StringVar(&repository, "repository", "", `Set repository`)
+	artifactoryCmd.Flags().StringVar(&artifactoryAction, "action", "", "Possible actions: repository-list, artifact-list, not-used-since, delete-artifact, get-info")
 }
 
 func artifactoryRun(cmd *cobra.Command, args []string) {
 	switch artifactoryAction {
-	case "artifact-list":
-		validate(repository)
-		path := args[0]
-		repositories := artifactory.GetArtifactsOfRepository(repository, path)
+	case "repository-list":
+		repositories := artifactory.GetRepositories()
 		fmt.Println(output.AnyToString(repositories))
+		return
+	case "artifact-list":
+		if !valid(repository) { repository = args[0] }
+		artifacts := artifactory.GetArtifacts(repository)
+		fmt.Println(output.AnyToString(artifacts))
 		return
 	case "not-used-since":
-		validate(repository)
+		if !valid(repository) { return }
 		notUsedSince := args[0]
-		repositories := artifactory.GetArtifactNotUsedSinceForRepository(repository, notUsedSince)
-		fmt.Println(output.AnyToString(repositories))
+		artifacts := artifactory.GetArtifactNotUsedSinceForRepository(repository, notUsedSince)
+		fmt.Println(output.AnyToString(artifacts))
 		return
 	case "delete-artifact":
-		validate(repository)
-		validate(artifact)
+		if !valid(artifact) { return }
+		if !valid(repository) { return }
 		result := artifactory.DeleteArtifact(repository, artifact)
 		if (result) { 
 			fmt.Println("INFO: Deleted") 
@@ -61,17 +73,28 @@ func artifactoryRun(cmd *cobra.Command, args []string) {
 			fmt.Println("ERR: Unable to delete") 
 		}
 		return
+	case "get-info":
+		if !valid(repository) { return }
+		if !valid(args[0]) { return }
+		items := artifactory.GetItemInfos(repository, args[0])
+		fmt.Println(output.AnyToString(items))
+		return
 	default:
-		validate(artifact)
-		artifactInfo := artifactory.GetArtifactByName(artifact)
-		fmt.Println(output.AnyToString(artifactInfo))
+		if !valid(repository) { 
+			fmt.Println("ERR: At least a repository must be passed") 
+			return 
+		}
+		path := repository + "/" + artifact
+		artifact := artifactory.GetArtifacts(path)
+		fmt.Println(output.AnyToString(artifact))
 		return
 	}
 }
 
-func validate(element string)  {
+func valid(element string) bool {
 	if len(element) == 0 {
 		fmt.Println("[Err]: All the necessary parameters must be pass")
-		return
+		return false
 	}
+	return true
 }
